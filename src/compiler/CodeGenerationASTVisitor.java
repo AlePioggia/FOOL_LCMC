156ff6b4961b810c31c1125709e5fdebaf500296
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-04-29 12:39:09 +0200

    order code generation methods

--- src/compiler/CodeGenerationASTVisitor.java
+++ src/compiler/CodeGenerationASTVisitor.java
@@ -89,106 +89,6 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 		return "push "+funl;		//indirizzo della funzione da ritornare
 	}
 
-	/**
-	 * Differenza con funNode -> non ritorna il push label ma l'etichetta che genera la mette nel suo campo label.
-	 * Perché verrà usata a livello di class node, per popolare la dispatch table.
-	 * */
-	@Override
-	public String visitNode(MethodNode n) throws VoidException {
-		if (print) printNode(n, n.id);
-		String declCode = null, popDecl = null, popParl = null;
-		for (Node dec : n.declist) {
-			declCode = nlJoin(declCode,visit(dec));
-			popDecl = nlJoin(popDecl,"pop");
-		}
-		for (int i=0;i<n.parlist.size();i++) popParl = nlJoin(popParl,"pop");
-		String generatedCode = freshFunLabel();
-		putCode(
-				nlJoin(
-						generatedCode +":",
-						"cfp", // set $fp to $sp value
-						"lra", // load $ra value
-						declCode, // generate code for local declarations (they use the new $fp!!!)
-						visit(n.exp), // generate code for function body expression
-						"stm", // set $tm to popped value (function result)
-						popDecl, // remove local declarations from stack
-						"sra", // set $ra to popped value
-						"pop", // remove Access Link from stack
-						popParl, // remove parameters from stack
-						"sfp", // set $fp to popped value (Control Link)
-						"ltm", // load $tm value (function result)
-						"lra", // load $ra value
-						"js"  // jump to popped address
-				)
-		);
-		n.label = generatedCode;
-		return null;
-	}
-
-	/**
-	 * Nel caso di null, sullo stack mettiamo -1, perché non è un indirizzo (non esiste l'indirizzo -1).
-	 * Vogliamo distinguere null da qualsiasi altro oggetto, se facciamo new A() == null, vogliamo che torni falso,
-	 * gli id non potranno mai coincidere.
-	 *
-	 * */
-	@Override
-	public String visitNode(EmptyNode n) throws VoidException {
-		if (print) printNode(n); return "push -1";
-	}
-
-	/**
-	 * Dichiarazione Classe: codice ritornato
-	 * 1. metto valore di $hp sullo stack: sarà il dispatch
-	 * pointer da ritornare alla fine
-	 * */
-	/**
-	 *2. Creo sullo heap la dispatch table,
-	 *  lavorando sullo stack, ogni volta, parto dalla posizione 0
-	 *  della dispatch table (dove c'è il primo metodo),
-	 *  caricherò la sua etichetta sullo stack e dovrò
-	 *  poi fare in modo che questa etichetta finisca nell'indirizzo hp.
-	 * Uso le istruzioni della nostra vm per mettere la label dentro hp.
-	 *
-	 * */
-	@Override
-	public String visitNode(ClassNode n)  {
-		//DISPATCH TABLE CREATION
-
-		if (print) printNode(n, n.classId);
-		//table which contains addresses to class methods
-		List<String> dispatchTable = new ArrayList<>();
-		if (n.superId != null) {
-			//dispatch table of inherited class
-			dispatchTable.addAll(dispatchTables.get(-n.superEntry.offset - 2));
-		}
-		this.dispatchTables.add(dispatchTable);
-		//add address for each method, it's needed to visit it first
-		for (var method: n.methods) {
-			visit(method);
-			dispatchTable.add(method.label);
-		}
-		String codeGeneration = "";
-		String loadHp = "lhp"; //Used to put hp value in stack, it will be the dispatch pointer to return (last available position)
-
-		// LOAD DISPATCH TABLE IN HEAP
-		for(var label: dispatchTable) { // loop through all dispatch table labels
-			codeGeneration = nlJoin(
-					codeGeneration,
-					"push " + label,
-					"lhp",			//push(hp)
-					"sw",			// 1. address = pop() -> takes hp value, then memory[address] = pop(), to put label in heap
-
-					"lhp",			//increment hp
-					"push 1",
-					"add",
-
-					"shp"			//hp = pop(), stores new hp address in stack, to setup for next cycle
-			);
-		}
-
-		return nlJoin(loadHp, codeGeneration);
-	}
-
 	/** sufficiente visitarla, per mettere il risultato sullo stack*/
 	@Override
 	public String visitNode(VarNode n) {
@@ -205,25 +105,25 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 	public String visitNode(PrintNode n) {
 		if (print) printNode(n);
 		return nlJoin(
-			visit(n.exp),
-			"print"
+				visit(n.exp),
+				"print"
 		);
 	}
 
 	@Override
 	public String visitNode(IfNode n) {
 		if (print) printNode(n);
-	 	String l1 = freshLabel();
-	 	String l2 = freshLabel();		
+		String l1 = freshLabel();
+		String l2 = freshLabel();
 		return nlJoin(
-			visit(n.cond),
-			"push 1", // aggiungo 1, ovvero true per confrontarlo con la condizione
-			"beq "+l1, // se la condizione è vera salto ad l1, dunque visito e ritorno il then
-			visit(n.el), // qui la condizione è falsa e ritorno l'else
-			"b "+l2,
-			l1+":",
-			visit(n.th),
-			l2+":"
+				visit(n.cond),
+				"push 1", // aggiungo 1, ovvero true per confrontarlo con la condizione
+				"beq "+l1, // se la condizione è vera salto ad l1, dunque visito e ritorno il then
+				visit(n.el), // qui la condizione è falsa e ritorno l'else
+				"b "+l2,
+				l1+":",
+				visit(n.th),
+				l2+":"
 		);
 	}
 
@@ -236,17 +136,17 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 	@Override
 	public String visitNode(EqualNode n) {
 		if (print) printNode(n);
-	 	String l1 = freshLabel(); //genero la nuova etichetta a cui saltare
-	 	String l2 = freshLabel();
+		String l1 = freshLabel(); //genero la nuova etichetta a cui saltare
+		String l2 = freshLabel();
 		return nlJoin(
-			visit(n.left),
-			visit(n.right),
-			"beq "+l1, //confronta due elementi, se sono uguali va ad l1, se sono diversi tira dritto
-			"push 0", // false
-			"b "+l2, // salto incondizionato
-			l1+":",
-			"push 1", // true
-			l2+":"
+				visit(n.left),
+				visit(n.right),
+				"beq "+l1, //confronta due elementi, se sono uguali va ad l1, se sono diversi tira dritto
+				"push 0", // false
+				"b "+l2, // salto incondizionato
+				l1+":",
+				"push 1", // true
+				l2+":"
 		);
 	}
 
@@ -254,10 +154,10 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 	public String visitNode(TimesNode n) {
 		if (print) printNode(n);
 		return nlJoin(
-			visit(n.left),
-			visit(n.right),
-			"mult"
-		);	
+				visit(n.left),
+				visit(n.right),
+				"mult"
+		);
 	}
 
 	/* Genero il codice di left, genero il codice di right, poi somma.
@@ -266,129 +166,9 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 	public String visitNode(PlusNode n) {
 		if (print) printNode(n);
 		return nlJoin(
-			visit(n.left),
-			visit(n.right),
-			"add"				
-		);
-	}
-
-	@Override
-	public String visitNode(LessEqualNode n) throws VoidException {
-		if (print) printNode(n);
-		String l1 = freshLabel();
-		String l2 = freshLabel();
-		return nlJoin(
 				visit(n.left),
 				visit(n.right),
-				"bleq " + l1, //mi chiedo se left <= right
-				"push 0", // false
-				"b " + l2, // salto incondizionato
-				l1 + ":",
-				"push 1", // true
-				l2 + ":"
-		);
-	}
-
-	@Override
-	public String visitNode(GreaterEqualNode n) throws VoidException {
-		if (print) printNode(n);
-		var l1 = freshLabel();
-		var l2 = freshLabel();
-		return nlJoin(
-				visitNode(new EqualNode(n.left, n.right)), //checks if they're equal
-																// , the result will be on top of the stack
-				"push 1",
-				"beq " + l2,
-				visit(n.left),
-				visit(n.right),
-				"bleq " + l1, // controllo se left < right, in quel caso è false
-				"push 1",
-				"b " + l2,
-				l1 + ":",
-				"push 0",
-				l2 + ":"
-		);
-	}
-
-	@Override
-	public String visitNode(NotNode n) throws VoidException {
-		if (print) printNode(n);
-		var l1 = freshLabel();
-		var l2 = freshLabel();
-
-		return nlJoin(
-				visit(n.node),
-				"push 1",
-				"beq " + l1,
-				"push 1",
-				"b " + l2,
-				l1 + ":",
-				"push 0",
-				l2 + ":"
-		);
-	}
-
-	@Override
-	public String visitNode(MinusNode n) throws VoidException {
-		if (print) printNode(n);
-		return nlJoin(
-				visit(n.left),
-				visit(n.right),
-				"sub"
-		);
-	}
-
-	@Override
-	public String visitNode(OrNode n) throws VoidException {
-		if (print) printNode(n);
-		var l1 = freshLabel();
-		var l2 = freshLabel();
-		return nlJoin(
-				visit(n.left),
-				"push 1",
-				"beq " + l1,
-				visit(n.right),
-				"push 1",
-				"beq " + l1,
-				"push 0",
-				"b " + l2,
-				l1 + ":",
-				"push 1",
-				l2 + ":"
-		);
-	}
-
-	@Override
-	public String visitNode(DivNode n) throws VoidException {
-		if (print) printNode(n);
-		return nlJoin(
-				visit(n.left),
-				visit(n.right),
-				"div"
-		);
-	}
-
-	@Override
-	public String visitNode(AndNode n) throws VoidException {
-		if (print) printNode(n);
-		var l1 = freshLabel();
-		var l2 = freshLabel();
-		var l3 = freshLabel();
-		return nlJoin(
-			visit(n.left),
-			"push 1",
-			"beq " + l1,
-			"push 0",
-			"b " + l3,
-			l1+":",
-			visit(n.right),
-			"push 1",
-			"beq " + l2,
-			"push 0",
-			"b " + l3,
-			l2+ ":",
-			"push 1",
-			l3+ ":"
+				"add"
 		);
 	}
 
@@ -468,8 +248,269 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 		);
 	}
 
+	/**
+	 * sullo stack abbiamo messo l'indirizzo della seconda variabile
+	 * con lw prendo il valore della variabile, perché avevamo fino ad ora usato solo l'indirizzo
+	 *
+	 * Come faccio a capire la differenza fra uso e dichiarazione?
+	 * n.entry.nl è il nesting level della dichiarazione
+	 * n.nl è invece il nesting level dell'uso
+	 * Lo abbiamo aggiunto nella symbol table
+	 *
+	 * Dopodiché, molto semplicemente aggiungo all'address, l'offset per recuperare la dichiarazione
+	 *
+	 * 1. Carica sullo stack il frame pointer, punta al frame locale;
+	 * 2. Gestisco la risalita con getAR, è sufficiente deferenziare con lw, punta sempre all'access link
+	 * 3. Applico l'offset all'indirizzo
+	 * 4. Prendo il valore della variabile dichiarata (dallo heap, attraverso l'address) e lo metto sullo stack
+	 *
+	 * */
+	@Override
+	public String visitNode(IdNode n) {
+		if (print) printNode(n,n.id);
+		String getAR = null;
+		for (int i = 0;i<n.nl-n.entry.nl;i++) getAR=nlJoin(getAR,"lw"); // faccio svariati load word, uno per variabile
+		return nlJoin(
+				"lfp", getAR, // retrieve address of frame containing "id" declaration
+				// by following the static chain (of Access Links)
+				"push "+n.entry.offset,
+				"add", // compute address of "id" declaration
+				"lw" // load value of "id" variable
+		);
+	}
+
+	@Override
+	public String visitNode(BoolNode n) {
+		if (print) printNode(n,n.val.toString());
+		return "push "+(n.val?1:0);
+	}
+
+	/**pusho l'intero, lo aggiungo*/
+	@Override
+	public String visitNode(IntNode n) {
+		if (print) printNode(n,n.val.toString());
+		return "push "+n.val;
+	}
+
+	@Override
+	public String visitNode(GreaterEqualNode n) throws VoidException {
+		if (print) printNode(n);
+		var l1 = freshLabel();
+		var l2 = freshLabel();
+		return nlJoin(
+				visitNode(new EqualNode(n.left, n.right)), //checks if they're equal
+				// , the result will be on top of the stack
+				"push 1",
+				"beq " + l2,
+				visit(n.left),
+				visit(n.right),
+				"bleq " + l1, // controllo se left < right, in quel caso è false
+				"push 1",
+				"b " + l2,
+				l1 + ":",
+				"push 0",
+				l2 + ":"
+		);
+	}
+
+	@Override
+	public String visitNode(LessEqualNode n) throws VoidException {
+		if (print) printNode(n);
+		String l1 = freshLabel();
+		String l2 = freshLabel();
+		return nlJoin(
+				visit(n.left),
+				visit(n.right),
+				"bleq " + l1, //mi chiedo se left <= right
+				"push 0", // false
+				"b " + l2, // salto incondizionato
+				l1 + ":",
+				"push 1", // true
+				l2 + ":"
+		);
+	}
+
+	@Override
+	public String visitNode(NotNode n) throws VoidException {
+		if (print) printNode(n);
+		var l1 = freshLabel();
+		var l2 = freshLabel();
+
+		return nlJoin(
+				visit(n.node),
+				"push 1",
+				"beq " + l1,
+				"push 1",
+				"b " + l2,
+				l1 + ":",
+				"push 0",
+				l2 + ":"
+		);
+	}
+
+	@Override
+	public String visitNode(MinusNode n) throws VoidException {
+		if (print) printNode(n);
+		return nlJoin(
+				visit(n.left),
+				visit(n.right),
+				"sub"
+		);
+	}
+
+	@Override
+	public String visitNode(OrNode n) throws VoidException {
+		if (print) printNode(n);
+		var l1 = freshLabel();
+		var l2 = freshLabel();
+		return nlJoin(
+				visit(n.left),
+				"push 1",
+				"beq " + l1,
+				visit(n.right),
+				"push 1",
+				"beq " + l1,
+				"push 0",
+				"b " + l2,
+				l1 + ":",
+				"push 1",
+				l2 + ":"
+		);
+	}
+
+	@Override
+	public String visitNode(DivNode n) throws VoidException {
+		if (print) printNode(n);
+		return nlJoin(
+				visit(n.left),
+				visit(n.right),
+				"div"
+		);
+	}
+
+	@Override
+	public String visitNode(AndNode n) throws VoidException {
+		if (print) printNode(n);
+		var l1 = freshLabel();
+		var l2 = freshLabel();
+		var l3 = freshLabel();
+		return nlJoin(
+				visit(n.left),
+				"push 1",
+				"beq " + l1,
+				"push 0",
+				"b " + l3,
+				l1+":",
+				visit(n.right),
+				"push 1",
+				"beq " + l2,
+				"push 0",
+				"b " + l3,
+				l2+ ":",
+				"push 1",
+				l3+ ":"
+		);
+	}
+	/**
+	 * Nel caso di null, sullo stack mettiamo -1, perché non è un indirizzo (non esiste l'indirizzo -1).
+	 * Vogliamo distinguere null da qualsiasi altro oggetto, se facciamo new A() == null, vogliamo che torni falso,
+	 * gli id non potranno mai coincidere.
+	 *
+	 * */
+
+	/**
+	 * Dichiarazione Classe: codice ritornato
+	 * 1. metto valore di $hp sullo stack: sarà il dispatch
+	 * pointer da ritornare alla fine
+	 * */
+	/**
+	 *2. Creo sullo heap la dispatch table,
+	 *  lavorando sullo stack, ogni volta, parto dalla posizione 0
+	 *  della dispatch table (dove c'è il primo metodo),
+	 *  caricherò la sua etichetta sullo stack e dovrò
+	 *  poi fare in modo che questa etichetta finisca nell'indirizzo hp.
+	 * Uso le istruzioni della nostra vm per mettere la label dentro hp.
+	 *
+	 * */
+
+	@Override
+	public String visitNode(ClassNode n)  {
+		//DISPATCH TABLE CREATION
+
+		if (print) printNode(n, n.classId);
+		//table which contains addresses to class methods
+		List<String> dispatchTable = new ArrayList<>();
+		if (n.superId != null) {
+			//dispatch table of inherited class
+			dispatchTable.addAll(dispatchTables.get(-n.superEntry.offset - 2));
+		}
+		this.dispatchTables.add(dispatchTable);
+		//add address for each method, it's needed to visit it first
+		for (var method: n.methods) {
+			visit(method);
+			dispatchTable.add(method.label);
+		}
+		String codeGeneration = "";
+		String loadHp = "lhp"; //Used to put hp value in stack, it will be the dispatch pointer to return (last available position)
+
+		// LOAD DISPATCH TABLE IN HEAP
+		for(var label: dispatchTable) { // loop through all dispatch table labels
+			codeGeneration = nlJoin(
+					codeGeneration,
+					"push " + label,
+					"lhp",			//push(hp)
+					"sw",			// 1. address = pop() -> takes hp value, then memory[address] = pop(), to put label in heap
+
+					"lhp",			//increment hp
+					"push 1",
+					"add",
+
+					"shp"			//hp = pop(), stores new hp address in stack, to setup for next cycle
+			);
+		}
+
+		return nlJoin(loadHp, codeGeneration);
+	}
+
+	/**
+	 * Differenza con funNode -> non ritorna il push label ma l'etichetta che genera la mette nel suo campo label.
+	 * Perché verrà usata a livello di class node, per popolare la dispatch table.
+	 * */
+	@Override
+	public String visitNode(MethodNode n) throws VoidException {
+		if (print) printNode(n, n.id);
+		String declCode = null, popDecl = null, popParl = null;
+		for (Node dec : n.declist) {
+			declCode = nlJoin(declCode,visit(dec));
+			popDecl = nlJoin(popDecl,"pop");
+		}
+		for (int i=0;i<n.parlist.size();i++) popParl = nlJoin(popParl,"pop");
+		String generatedCode = freshFunLabel();
+		putCode(
+				nlJoin(
+						generatedCode +":",
+						"cfp", // set $fp to $sp value
+						"lra", // load $ra value
+						declCode, // generate code for local declarations (they use the new $fp!!!)
+						visit(n.exp), // generate code for function body expression
+						"stm", // set $tm to popped value (function result)
+						popDecl, // remove local declarations from stack
+						"sra", // set $ra to popped value
+						"pop", // remove Access Link from stack
+						popParl, // remove parameters from stack
+						"sfp", // set $fp to popped value (Control Link)
+						"ltm", // load $tm value (function result)
+						"lra", // load $ra value
+						"js"  // jump to popped address
+				)
+		);
+		n.label = generatedCode;
+		return null;
+	}
+
 	// id1.id2()
 	// A a = new A(); a -> A
+
 	/**
 	 * Differences with CallNode
 	 * - id1 is the object pointer, and it's useful to
@@ -504,42 +545,11 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 		);
 	}
 
-	/**
-	 * sullo stack abbiamo messo l'indirizzo della seconda variabile
-	 * con lw prendo il valore della variabile, perché avevamo fino ad ora usato solo l'indirizzo
-	 *
-	 * Come faccio a capire la differenza fra uso e dichiarazione?
-	 * n.entry.nl è il nesting level della dichiarazione
-	 * n.nl è invece il nesting level dell'uso
-	 * Lo abbiamo aggiunto nella symbol table
-	 *
-	 * Dopodiché, molto semplicemente aggiungo all'address, l'offset per recuperare la dichiarazione
-	 *
-	 * 1. Carica sullo stack il frame pointer, punta al frame locale;
-	 * 2. Gestisco la risalita con getAR, è sufficiente deferenziare con lw, punta sempre all'access link
-	 * 3. Applico l'offset all'indirizzo
-	 * 4. Prendo il valore della variabile dichiarata (dallo heap, attraverso l'address) e lo metto sullo stack
-	 *
-	 * */
-	@Override
-	public String visitNode(IdNode n) {
-		if (print) printNode(n,n.id);
-		String getAR = null;
-		for (int i = 0;i<n.nl-n.entry.nl;i++) getAR=nlJoin(getAR,"lw"); // faccio svariati load word, uno per variabile
-		return nlJoin(
-			"lfp", getAR, // retrieve address of frame containing "id" declaration
-			              // by following the static chain (of Access Links)
-			"push "+n.entry.offset,
-			"add", // compute address of "id" declaration
-			"lw" // load value of "id" variable
-		);
-	}
-
 	/*
-	* Key aspects:
-	* - dispatch pointer -> points to the dispatch table;
-	* - object pointer -> points to the object (in heap), which inside contains dispatch pointer, to access methods;
-	* */
+	 * Key aspects:
+	 * - dispatch pointer -> points to the dispatch table;
+	 * - object pointer -> points to the object (in heap), which inside contains dispatch pointer, to access methods;
+	 * */
 	@Override
 	public String visitNode(NewNode n) {
 		if (print) printNode(n, n.id);
@@ -556,7 +566,7 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 					"push 1",
 					"add",
 					"shp"
-					);
+			);
 		}
 
 		return nlJoin(
@@ -583,15 +593,7 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 	}
 
 	@Override
-	public String visitNode(BoolNode n) {
-		if (print) printNode(n,n.val.toString());
-		return "push "+(n.val?1:0);
-	}
-
-	/**pusho l'intero, lo aggiungo*/
-	@Override
-	public String visitNode(IntNode n) {
-		if (print) printNode(n,n.val.toString());
-		return "push "+n.val;
+	public String visitNode(EmptyNode n) throws VoidException {
+		if (print) printNode(n); return "push -1";
 	}
 }
